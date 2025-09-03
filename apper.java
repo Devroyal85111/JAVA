6b8752e38da211e81a6aa60e24da7da52bf5755a
your name <you@example.com>
2025-09-03 15:53:32 +0530

    apper

---
+++ apper.java
@@ -0,0 +1,15 @@
+ import java.util.*;
+public class apper{
+//a=97,A=65
+    public static void main(String[] ags){
+        char ch='A';
+        if(ch>='a' && ch<='z')
+        {
+            System.out.println("It is  lower case");
+            
+        }
+        else if(ch<'A' && ch<='Z'){
+            System.out.println("It is Upper case");
+        }
+    }
+}
---
+++ forloop.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class forloop{
+    public static void main(String[] args){
+        int i;
+        System.out.println(Enter the num:-);
+        
+        for(i=100;i>=1;i--)
+        {
+            System.out.print(i+"\t");
+        }
+    }
+}
